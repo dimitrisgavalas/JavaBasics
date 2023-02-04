91a669e96ca5c96039760169fc5c5b4bce7bb355
dimitrisgavalas <dimitrisgavalas97@gmail.com>
2023-02-04 21:35:14 +0200

    Added a different Implementation of master Challenge Hamburger class

--- src/MasterChallenge/Main.java
+++ src/MasterChallenge/Main.java
@@ -3,10 +3,20 @@ package MasterChallenge;
 public class Main {
     public static void main(String[] args) {
 
-        Hamburger hamburger = new Hamburger("Basic", "Cow", 15, "BB");
-        hamburger.setKetchup(true);
-        hamburger.setTomato(true);
+//        Hamburger hamburger = new Hamburger("Basic", "Cow", 15, "BB");
+//        hamburger.setKetchup(true);
+//        hamburger.setTomato(true);
+//
+//        hamburger.totalPrice();
 
-        hamburger.totalPrice();
+        Hamburger2 hamburger2 = new Hamburger2("Basic", "Pork", 3.58, "White");
+        double price = hamburger2.itemizeHamburger();
+        hamburger2.addHamburgerAddition1("Tomato", 0.3);
+        hamburger2.addHamburgerAddition2("Lettuce", 0.3);
+        hamburger2.addHamburgerAddition3("Cheese", 0.3);
+
+        System.out.println("Total burger price is " + hamburger2.itemizeHamburger());
+
+
     }
 }
