a403cc68ccdfcfa9801cd990d4aee18bd4e8cdc7
dimitrisgavalas <dimitrisgavalas97@gmail.com>
2023-02-10 23:24:06 +0200

    Added Reverse array method and class

--- src/ArrayLists/Main.java
+++ src/ArrayLists/Main.java
@@ -1,5 +1,6 @@
 package ArrayLists;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -14,6 +15,21 @@ public class Main {
         }
 
         System.out.println("The average is " + getAverage(myInts));
+
+//        Min in array
+        System.out.println("How many integers do you want to add to your array?");
+        int count = scanner.nextInt();
+
+        int array[] = readIntegers(count);
+        int min = findMin(array);
+        System.out.println("Min of array is = " + min);
+
+//      Reversed Array
+        int[] array2 = {1,2,3,4,5};
+        System.out.println("Array = " + Arrays.toString(array2));
+        reverse(array2);
+        System.out.println("Reversed Array = " + Arrays.toString(array2));
+
 
     }
 
@@ -38,4 +54,37 @@ public class Main {
     }
 
 
+    public static int[] readIntegers(int count){
+        System.out.println("Enter " + count + " integers:");
+        int[] array = new int[count];
+
+        for (int i=0; i<array.length;i++){
+            System.out.println("Enter number");
+            array[i] = scanner.nextInt();
+        }
+        return array;
+    }
+
+    public static int findMin(int[] array){
+
+        int min = Integer.MAX_VALUE;
+        for (int i=0; i<array.length;i++){
+            if (array[i] < min){
+                min = array[i];
+            }
+        }
+
+        return min;
+    }
+
+    private static void reverse(int[] array){
+
+        int maxIndex = array.length - 1;
+        int halfLength = array.length / 2;
+        for (int i=0; i< halfLength; i++){
+            int temp = array[i];
+            array[i] = array[maxIndex - i];
+            array[maxIndex - i] = temp;
+        }
+    }
 }
---
+++ src/ArrayLists/MinimumElement.java
@@ -0,0 +1,42 @@
+package ArrayLists;
+
+import java.util.Scanner;
+public class MinimumElement {
+
+    //private Scanner scanner = new Scanner(System.in);
+
+    // write code here
+    private static int readInteger(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enetr how many spaces do you want the array to have: ");
+        int arrayLength = scanner.nextInt();
+
+        return arrayLength;
+    }
+
+    private static int[] readElements(int count){
+        Scanner scanner2 = new Scanner(System.in);
+        int[] array = new int[count];
+
+        for (int i = 0; i < count; i++){
+            System.out.println("Entero digit numero" + i + " : ");
+            array[i] = scanner2.nextInt();
+        }
+
+        return array;
+    }
+
+    private static int findMin(int[] array){
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < array.length; i++){
+            if (array[i] < min){
+                min = array[i];
+            }
+        }
+
+        return min;
+
+    }
+
+}
