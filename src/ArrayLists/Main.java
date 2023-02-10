103fd9bbd7abf1ce21f33714c8ee5d3666234643
dimitrisgavalas <dimitrisgavalas97@gmail.com>
2023-02-10 23:24:06 +0200

    Added minimum Element Challenge

--- src/ArrayLists/Main.java
+++ src/ArrayLists/Main.java
@@ -8,12 +8,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int [] myInts = getIntegers(5);
-        for (int i=0; i<myInts.length; i++){
-            System.out.println("Element " + i + ", typed value was " + myInts[i]);
-        }
+//        int [] myInts = getIntegers(5);
+//        for (int i=0; i<myInts.length; i++){
+//            System.out.println("Element " + i + ", typed value was " + myInts[i]);
+//        }
+//
+//        System.out.println("The average is " + getAverage(myInts));
 
-        System.out.println("The average is " + getAverage(myInts));
+        System.out.println("How many integers do you want to add to your array?");
+        int count = scanner.nextInt();
+
+        int array[] = readIntegers(count);
+        int min = findMin(array);
+        System.out.println("Min of array is = " + min);
 
     }
 
@@ -38,4 +45,25 @@ public class Main {
     }
 
 
+    public static int[] readIntegers(int count){
+        System.out.println("Enter " + count + " integers:");
+        int[] array = new int[count];
+
+        for (int i=0; i<array.length;i++){
+            array[i] = scanner.nextInt();
+        }
+        return array;
+    }
+
+    public static int findMin(int[] array){
+
+        int min = Integer.MAX_VALUE;
+        for (int i=0; i<array.length;i++){
+            if (array[i] < min){
+                min = array[i];
+            }
+        }
+
+        return min;
+    }
 }
