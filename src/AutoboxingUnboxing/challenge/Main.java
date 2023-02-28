d87fe114b5271522545c649a274af72ea316e2e9
dimitrisgavalas <dimitrisgavalas97@gmail.com>
2023-02-28 23:26:42 +0200

    Added  Autoboxing Unboxing Challenge

--- src/AutoboxingUnboxing/challenge/Main.java
+++ src/AutoboxingUnboxing/challenge/Main.java
@@ -4,5 +4,27 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Bank bank = new Bank("NAB");
+
+        Branch branch = new Branch("Adelaide");
+
+        bank.addCustomer("Adelaide", "Tim", 50.05);
+        bank.addCustomer("Adelaide", "Mike", 175.34);
+        bank.addCustomer("Adelaide", "Percy", 220.12);
+
+        bank.addBranch("Sydney");
+        bank.addCustomer("Sydney", "Bob", 150.54);
+
+        bank.addCustomerTransaction("Adelaide", "Tim", 44.22);
+        bank.addCustomerTransaction("Adelaide", "Tim", 12.24);
+        bank.addCustomerTransaction("Adelaide", "Mike", 1.65);
+
+        bank.listCustomers("Adelaide", true);
+
+
+
+
+
+
     }
 }
